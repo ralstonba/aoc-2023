203a3aae5c54046f927666bc2a95bd67cbefda11
Brendan Ralston <brendan@ralston.io>
2023-12-01 23:19:54 -0500

    Part 2 solution using hack string replacement

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,4 +1,17 @@
 fun main() {
+
+    val MAPPING = mapOf(
+        "one" to "on1e",
+        "two" to "tw2o",
+        "three" to "thr3ee",
+        "four" to "fo4ur",
+        "five" to "fi5ve",
+        "six" to "si6x",
+        "seven" to "se7en",
+        "eight" to "ei8ht",
+        "nine" to "ni9ne"
+    )
+
     fun calibrationData(input: String): Int {
         val first = input.first { it.isDigit() }
         val last = input.last { it.isDigit() }
@@ -6,12 +19,21 @@ fun main() {
         return "$first$last".toInt()
     }
 
+    fun replaceTextualNumbers(input: String): String {
+        var out = input
+        for (e in MAPPING.entries) {
+            out = out.replace(e.key, e.value)
+        }
+
+        return out
+    }
+
     fun part1(input: List<String>): Int {
         return input.sumOf { calibrationData(it) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf { calibrationData(replaceTextualNumbers(it)) }
     }
 
     // test if implementation meets criteria from the description, like:
@@ -30,7 +52,7 @@ fun main() {
     check(part1(testInput) == 142)
     check(part2(testInputPart2) == 281)
 
-    val input = readInput("day01.txt")
+    val input = readInput("day01")
     part1(input).println()
     part2(input).println()
 }
