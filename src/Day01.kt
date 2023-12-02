561e4203d9853abb951674cb40a9b56dbc6110dd
Brendan Ralston <brendan@ralston.io>
2023-12-01 19:52:48 -0500

    Part 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,13 @@
 fun main() {
+    fun calibrationData(input: String): Int {
+        val first = input.first { it.isDigit() }
+        val last = input.last { it.isDigit() }
+
+        return "$first$last".toInt()
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { calibrationData(it) }
     }
 
     fun part2(input: List<String>): Int {
@@ -8,10 +15,22 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = listOf("1abc2",
+            "pqr3stu8vwx",
+            "a1b2c3d4e5f",
+            "treb7uchet")
+    val testInputPart2 = listOf("two1nine",
+            "eightwothree",
+            "abcone2threexyz",
+            "xtwone3four",
+            "4nineeightseven2",
+            "zoneight234",
+            "7pqrstsixteen")
 
-    val input = readInput("Day01")
+    check(part1(testInput) == 142)
+    check(part2(testInputPart2) == 281)
+
+    val input = readInput("day01.txt")
     part1(input).println()
     part2(input).println()
 }
