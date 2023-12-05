1cde1bddcba07254b1894de1b1375bc6f45f8e5e
Brendan Ralston <brendan@ralston.io>
2023-12-04 22:29:25 -0500

    Day 4, part 1

---
+++ src/Day04.kt
@@ -0,0 +1,45 @@
+fun main() {
+
+    data class Card(val number: Int, val mine: Set<Int>, val winning: Set<Int>) {
+        fun points(): Int {
+            return (1 shl (numberOfWinningNumbers() - 1)).coerceAtLeast(0)
+        }
+        
+        fun numberOfWinningNumbers(): Int {
+            return mine.filter { winning.contains(it) }.size
+        }
+    }
+
+    fun parse(input: String): Card {
+        val (card: String, have: String, winning: String) = input.split(":", "|")
+        val cardNumber = "\\d+".toRegex().find(card)?.value?.toInt() ?: 0
+        val myNumbers = have.split(" ").filter { it.isNotBlank() }.map { it.toInt() }.toSet()
+        val winningNums = winning.split(" ").filter { it.isNotBlank() }.map { it.toInt() }.toSet()
+        return Card(cardNumber, myNumbers, winningNums)
+    }
+
+    fun part1(input: List<String>): Int {
+        return input.sumOf { parse(it).points() }
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = listOf(
+        "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+        "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+        "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+        "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+        "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+        "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
+    )
+
+    check(part1(testInput) == 13)
+//    check(part2(testInput) == 2286)
+
+    val input = readInput("day04")
+    part1(input).println()
+    part2(input).println()
+}
